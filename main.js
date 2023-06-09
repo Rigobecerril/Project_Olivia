c78969ead690f675ec6af1b11d834e9ee5fd1752
Rigoberto Becerril Balderas <rigoberto.becerril.b@outlook.com>
2023-06-09 17:09:01 -0600

    .

--- main.js
+++ main.js
@@ -1,4 +1,16 @@
-$(document).ready(function(){
-   $('ul.tabs li a:first').addClass('active');
-   $('secciones article:first').show();
-});
+const. targets = document.querySelectorAll('[data-target]')
+const content = document.querySelectorAll('[data-content]')
+
+targets.forEach(target => {
+  target.addEventListener('click', () => {
+        content.forEach(c =>{
+            c.classList.remove('active')
+        
+        })
+        
+        const t = document.querySelector(target.dataset.target)
+        t.classList.add('active')
+    })
+    
+    
+})
